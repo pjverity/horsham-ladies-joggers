e4f668f9f944236628487cf60908cb560514cee3
Paul Verity <paul.verity@v-home.co.uk>
2018-04-30 22:53:00 +0100

    horsham-ladies-joggers-#7: Allow admins to amend schedules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
-import { h, render, Component } from 'preact';
-import Router from 'preact-router';
-import Match from 'preact-router/match';
+import { h, render, Component } from 'preact'
+import Router from 'preact-router'
+import Match from 'preact-router/match'
 
-import {FACEBOOK_URL, TWITTER_URL, SITE_EMAIL_ADDRESS} from '../site-constants';
+import {FACEBOOK_URL, SITE_EMAIL_ADDRESS} from '../site-constants'
 
-import Header from "./Header.jsx";
-import Main from "./Main.jsx";
-import Footer from "./Footer.jsx";
-import TokenClaimFailed from "./TokenClaimFailed.jsx";
-import TokenClaimOk from "./TokenClaimOk.jsx";
+import Header from "./Header.jsx"
+import Main from "./Main.jsx"
+import Footer from "./Footer.jsx"
+import Admin from "./Admin.jsx"
+import TokenClaimFailed from "./TokenClaimFailed.jsx"
+import TokenClaimOk from "./TokenClaimOk.jsx"
 
 export default class App extends Component {
 	render(props, state) {
@@ -18,12 +19,12 @@ export default class App extends Component {
 					{ ({ matches, path, url }) => (
 						<Header showLogo={path === '/'}
 						        emailAddress={SITE_EMAIL_ADDRESS}
-						        facebookUrl={FACEBOOK_URL}
-						        twitterUrl={TWITTER_URL}/>
+						        facebookUrl={FACEBOOK_URL}/>
 					) }
 				</Match>
 				<Router>
-					<Main exact path="/"/>
+					<Main exact path="/" />
+					<Admin exact path="/admin" />
 					<TokenClaimFailed path="/token-claim-failed"/>
 					<TokenClaimOk path="/token-claim-ok"/>
 				</Router>
